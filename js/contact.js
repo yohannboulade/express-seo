3bc054e7a16f2e12315d8115dea07425a194766e
Pili <pili@freshcore.fr>
2022-10-17 13:50:56 +0200

    ajout JS

--- js/contact.js
+++ js/contact.js
@@ -1,19 +1,5 @@
-const handleSubmit = (event) => {
-    event.preventDefault();
-  
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-    
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => console.log("Form successfully submitted"))
-      .catch((error) => alert(error));
-  };
-  
-  document
-    .querySelector("form")
-    .addEventListener("submit", handleSubmit);
-  
+form = document.getElementById('formContact');
+
+form.addEventListener('submit', function(){
+    alert('test');
+})
